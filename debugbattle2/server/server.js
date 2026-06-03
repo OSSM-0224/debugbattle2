c67d0c672e068d817135bd6efb9dad8015909f9a
Om Mhatre <108936280+OSSM-0224@users.noreply.github.com>
2026-06-03 22:55:02 +0530

    updating the cors and adding the vercel frontend app link

--- debugbattle2/server/server.js
+++ debugbattle2/server/server.js
@@ -1,29 +1,43 @@
-const express = require('express');
-const dotenv = require('dotenv');
-const cors = require('cors');
-const cookieParser = require('cookie-parser');
-const connectDB = require('./src/config/db');
-const { errorHandler } = require('./src/middleware/errorMiddleware');
-
-connectDB();
+const express = require("express");
+const dotenv = require("dotenv");
+const cors = require("cors");
+const cookieParser = require("cookie-parser");
+const connectDB = require("./src/config/db");
+const { errorHandler } = require("./src/middleware/errorMiddleware");
 dotenv.config();
+connectDB();
 
 const app = express();
 const port = process.env.PORT || 5000;
+const allowedOrigins = [
+  "http://localhost:5173",
+  "https://debugbattle2-zeta.vercel.app",
+];
 
-app.use(cors({ origin: 'http://localhost:5173', credentials: true }));
+app.use(
+  cors({
+    origin: (origin, callback) => {
+      if (!origin || allowedOrigins.includes(origin)) {
+        callback(null, true);
+      } else {
+        callback(new Error("Not allowed by CORS"));
+      }
+    },
+    credentials: true,
+  }),
+);
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 
-app.use('/api/auth', require('./src/routes/authRoutes'));
-app.use('/api/users', require('./src/routes/userRoutes'));
-app.use('/api/products', require('./src/routes/productRoutes'));
-app.use('/api/orders', require('./src/routes/orderRoutes'));
-app.use('/api/inventory', require('./src/routes/inventoryRoutes'));
+app.use("/api/auth", require("./src/routes/authRoutes"));
+app.use("/api/users", require("./src/routes/userRoutes"));
+app.use("/api/products", require("./src/routes/productRoutes"));
+app.use("/api/orders", require("./src/routes/orderRoutes"));
+app.use("/api/inventory", require("./src/routes/inventoryRoutes"));
 
-app.get('/', (req, res) => {
-  res.send('Server is running');
+app.get("/", (req, res) => {
+  res.send("Server is running");
 });
 
 app.use(errorHandler);
